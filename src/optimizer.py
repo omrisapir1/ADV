a1f59cd22c124622f2ed05ee6172d85b65dba847
Omri <omri.sapir@workday.com>
2025-11-18 17:11:45 +0200

    all

--- src/optimizer.py
+++ src/optimizer.py
@@ -7,21 +7,21 @@ def create_optimizer(model: torch.nn.Module, config: Dict[str, Any]) -> torch.op
     """Create optimizer based on config (no default values in .get)."""
     optim_config = config.get("optim")
 
-    # Parameters without weight decay
-    # no_decay_modules = optim_config.get("no_decay_modules")
-    #
-    # decay_params, no_decay_params = [], []
-    # for name, param in model.model.named_parameters():
-    #     if not param.requires_grad:
-    #         continue
-    #     should_not_decay = any(module_name in name for module_name in (no_decay_modules or []))
-    #     (no_decay_params if should_not_decay else decay_params).append(param)
-    #
-    # weight_decay_val = optim_config.get("weight_decay")
-    # param_groups = [
-    #     {"params": decay_params, "weight_decay": weight_decay_val},
-    #     {"params": no_decay_params, "weight_decay": 0.0},
-    # ]
+    Parameters without weight decay
+    no_decay_modules = optim_config.get("no_decay_modules")
+
+    decay_params, no_decay_params = [], []
+    for name, param in model.model.named_parameters():
+        if not param.requires_grad:
+            continue
+        should_not_decay = any(module_name in name for module_name in (no_decay_modules or []))
+        (no_decay_params if should_not_decay else decay_params).append(param)
+
+    weight_decay_val = optim_config.get("weight_decay")
+    param_groups = [
+        {"params": decay_params, "weight_decay": 0.0},
+        {"params": no_decay_params, "weight_decay": 0.0},
+    ]
 
     name_val = optim_config.get("name")
     if name_val.lower() == "adamw":
@@ -29,7 +29,7 @@ def create_optimizer(model: torch.nn.Module, config: Dict[str, Any]) -> torch.op
         betas_val = optim_config.get("betas")
         eps_val = optim_config.get("eps")
         optimizer = AdamW(
-            # param_groups,
+            param_groups,
             lr=float(lr_val),
             betas=betas_val,
             eps=float(eps_val),
