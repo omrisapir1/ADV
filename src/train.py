76b20070d84c81bd2660e53cb1fecc6c20bc0de8
Omri <omri.sapir@workday.com>
2025-12-17 10:50:52 +0200

    all

--- src/train.py
+++ src/train.py
@@ -417,7 +417,7 @@ async def training_loop(config: Dict[str, Any]):
         if alpha_state_path:
             alpha_control.load_state(alpha_state_path)
 
-        alpha_control.alpha = 0.25
+        alpha_control.alpha = 1
 
 
 
